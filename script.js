f53392517df50db0b96b7a7924bf96ec60407be1
diegoadrianm <156969054+diegoadrianm@users.noreply.github.com>
2024-02-06 21:45:54 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+var miVariable = 5;
+var campoTexto = document.getElementById("nombre");
+var texto = campoTexto.value;
+
+function miFuncion()
+{
+    var texto = campoTexto.value;
+    alert(texto);
+    
+}
+function clearText()
+{
+    document.getElementById('nombre').value = '';
+}
+
+
+
