f3d731cf551bf35be43767a4fcd429031a05cbf6
diegoadrianm <156969054+diegoadrianm@users.noreply.github.com>
2024-02-14 23:09:44 -0600

    Add files via upload

---
+++ ScriptP3.js
@@ -0,0 +1,13 @@
+
+var usuario = document.getElementById("User");
+var contrasegna = document.getElementsByName("Password");
+
+function LogIn()
+{
+    document.write("<div>Login correcto ", usuario, "</div>");
+}
+
+function SignIn()
+{
+    document.write("<div>Registro correcto ", usuario, "</div>");
+}
