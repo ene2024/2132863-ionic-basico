2b146b6cfa9e9d65f37040b1766fe583dae3bd8c
diegoadrianm <156969054+diegoadrianm@users.noreply.github.com>
2024-02-20 20:05:30 -0600

    Add files via upload

--- ScriptP3.js
+++ ScriptP3.js
@@ -1,13 +1,48 @@
 
-var usuario = document.getElementById("User");
-var contrasegna = document.getElementsByName("Password");
+/*var usuario = document.getElementById("User");
+var contrasegna = document.getElementById("Password");
+
+document.getElementById("b1").addEventListener("click", LogIn);
+document.getElementById("b1").addEventListener("click", SignIn);
 
 function LogIn()
 {
-    document.write("<div>Login correcto ", usuario, "</div>");
+    var respuesta = document.createElement("div");
+    var texto = document.createTextNode("Log in correcto");
+
+    respuesta.appendChild(texto);
+    document.getElementById("Impresion").appendChild(respuesta);
 }
 
 function SignIn()
 {
-    document.write("<div>Registro correcto ", usuario, "</div>");
+    var respuesta = document.createElement("div");
+    var texto = document.createTextNode("Registro correcto");
+
+    respuesta.appendChild(texto);
+    document.getElementById("Impresion").appendChild(respuesta);
+}*/
+
+
+
+var tipo = [];
+var precio = [];
+
+var boton = document.getElementById("boton");
+
+boton.addEventListener("click", guardar, limpiar);
+
+function guardar()
+{
+    tipo.push(document.getElementById("type"));
+    precio.push(document.getElementById("precio"));
 }
+
+function limpiar()
+{
+    document.getElementById('type').value = '';
+    document.getElementById('price').value = '';
+}
+
+
+
