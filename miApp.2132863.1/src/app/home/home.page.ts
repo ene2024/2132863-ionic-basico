4666376bc5c3be1a9dff933340899642670d6bd9
Diego Moreno <diegomorduar@gmail.com>
2024-03-22 20:17:40 -0600

    Práctica 7

---
+++ miApp.2132863.1/src/app/home/home.page.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  constructor() {}
+
+  alumnos = ["Carlos", "Ximena", "Alberto", "Eduardo", "Barbara", "Georgina", "Kevin"];
+
+}
