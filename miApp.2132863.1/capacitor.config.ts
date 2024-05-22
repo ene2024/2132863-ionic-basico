1448e2f7da180ef9dd18abfdfd1d5c5a949af7e7
Diego Moreno <diegomorduar@gmail.com>
2024-05-21 19:32:13 -0600

    practica 13

--- miApp.2132863.1/capacitor.config.ts
+++ miApp.2132863.1/capacitor.config.ts
@@ -1,12 +1,9 @@
-import { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.example.app',
   appName: 'miApp.2132863.1',
-  webDir: 'www',
-  server: {
-    androidScheme: 'https'
-  }
+  webDir: 'www'
 };
 
 export default config;
